08b4e6ce5f4860eb9d09e96e7f58234ff6efde27
Lauri Neuvonen <lauri.neuvonen@aalto.fi>
2021-04-21 21:32:08 +0300

    Pipeline runs through to building model and bold RDMs. Needs testing. Takes long.

--- pipeline.py
+++ pipeline.py
@@ -8,40 +8,61 @@ import nibabel as nib
 import numpy as np
 import pandas as pd
 import scipy
+from progress.bar import Bar
 from nilearn import plotting
 
 
-bold = nib.load('subj1/bold.nii.gz') # loads dataset of BOLD images in Nifti1Image format
-mask = nib.load('subj1/mask4_vt.nii.gz')
-data_shape = bold.shape
-voxel_shape = data_shape[0:3]
-voxel_index_grid = list(np.ndindex(voxel_shape))
-
-data = bold.get_fdata() #
-voxel_timecourse = data[20, 30, 30, :] # Nifti1Image -> Numpy array (example for time series of voxel at 20, 30, 30)
-
-anat = nib.load('subj1/anat.nii.gz')
-labels = pd.read_csv('subj1/labels.txt', sep=' ')
 
 def preprocess_chunk(chunk_data):
+    """This function preprocesses a chunk of data by first removing linear trend and the
+    z-scoring the data. Operations are performed on the last axis in data.
 
-    print(chunk_data[0:3, 0:3, 0:3, 0:10])
+    Parameters
+    ----------
+    chunk_data : array-like
+        NOTE: operations will be performed on the last axis.
+
+    Returns
+    -------
+    array-like
+        A preprocessed version of chunk data in same shape and format"""
+
     detrended = scipy.signal.detrend(chunk_data)
     z_scored = scipy.stats.zscore(detrended, axis=-1)
-    print(z_scored[0:3, 0:3, 0:3, 0:10])
 
     return z_scored
 
 def preprocess_voxel_data(bold_data, labels):
+    """Wrapper for handling preprocessing for a whole dataset comprised of chunks.
+
+    Parameters
+    ----------
+    bold_data : array-like
+        (NumPy) array containing neural image data
+
+    labels : Pandas dataframe
+        Should contain at least columns 'chunks' that matches in length to bold_data. This chunk information
+        is used to split bold_data into chunks, each pre-processed separately.
+
+    Returns
+    -------
+    array-like
+        A chunk-wise preprocessed version of bold_data in same shape and format
+    """
+
 
     processed_bold_np = np.zeros(bold_data.shape)
     chunks = np.unique(labels.chunks)
+
+    chunk_bar = Bar('Preprosessing chunks', max=len(chunks))
     for chunk in chunks:
         labels_chunk = labels.query(f'chunks == {chunk}')
         bold_chunk = bold_data[:, :, :, labels_chunk.index]
         preprocessed_chunk = preprocess_chunk(bold_chunk)
         processed_bold_np[:, :, :, labels_chunk.index] = preprocessed_chunk
+        chunk_bar.next()
 
+    chunk_bar.finish()
     return processed_bold_np
 
 def get_bold_in_roi(bold, roi_mask=nib.load('subj1/mask4_vt.nii.gz')):
@@ -81,17 +102,130 @@ def searchlight(center_voxels, radius, search_grid):
 
     return search_results
 
-# Plot data using a "glass brain"
-#some_data = bold.slicer[:, :, :, 0]  # First BOLD image
-#plotting.plot_glass_brain(some_data)
+
+def build_model_RDM(labels):
+
+    model_RDM = np.zeros((len(labels.index), len(labels.index)))
+    labels_sorted = labels.sort_values('labels')
 
 
-#voxels_in_roi = data[mask.get_fdata() == 1, :]
-#filter = mask.get_fdata() == 1
 
+def remove_rest(data, labels):
+    """Removes 'rest' labeled data.
+
+    Parameters
+    ----------
+    data : array-like
+        data to be cleaned
+
+    labels : Pandas dataframe
+        labels corresponding to last axis in data
+
+    Returns
+    -------
+    array-like
+        data that has been cleaned of data corresponding to 'rest' labels
+
+    Pandas dataframe
+        labels that correspond to returned data
+
+    """
+
+    clean_data = data[:, :, :, labels.query('labels != "rest" ').index]
+    clean_labels = labels[labels.labels != 'rest']
+
+    return clean_data, clean_labels
+
+def create_model_RDM(labels):
+    model_RDM = np.zeros((len(labels.index), len(labels.index)))
+
+    for i in range(len(labels_sorted.labels)):
+        for j in range(len(labels_sorted.labels)):
+            model_RDM[i, j] = (labels_sorted.labels.iloc[i] == labels_sorted.labels.iloc[j])
+
+    return model_RDM
+
+
+def flatten_voxels(data, locations, image):
+    flat = np.array([])
+    for loc in locations:
+        flat = np.append(flat, data[loc[0], loc[1], loc[2], image])
+
+    return flat
+
+
+def create_bold_RDM(data, location, ROI_searchlights):
+    n_images = len(data[0, 0, 0, :])
+    RDM = np.zeros((n_images, n_images))
+    search_locs = ROI_searchlights[location]
+
+    progress_bar = Bar('Single RDM progress', max=n_images)
+    for i in range(n_images):
+        for j in range(n_images):
+            image_1 = []
+            image_2 = []
+
+            image_1 = np.concatenate((image_1, flatten_voxels(data, search_locs, i)))
+            image_2 = np.concatenate((image_2, flatten_voxels(data, search_locs, j)))
+
+            RDM[i, j] = 1 - scipy.stats.pearsonr(image_1, image_2)[0]  # dissimilarity betw. 1 & 2
+        progress_bar.next()
+
+    progress_bar.finish()
+
+    return RDM
+
+# Input params
+radius = 2 # radius for searchlight
+
+# Load original data
+bold = nib.load('subj1/bold.nii.gz') # loads dataset of BOLD images in Nifti1Image format
+mask = nib.load('subj1/mask4_vt.nii.gz')
+anat = nib.load('subj1/anat.nii.gz')
+labels = pd.read_csv('subj1/labels.txt', sep=' ')
+
+data = bold.get_fdata()
+mask_data = mask.get_fdata()
+
+# Preprocessing
+print("Pre-processing...")
 processed_bold_data = preprocess_voxel_data(data, labels)
-print(processed_bold_data[0:3, 0:3, 0:3, 0:10])
 preprocessed_image = nib.Nifti1Image(processed_bold_data, bold.affine)
-#nib.save(preprocessed_image, 'images/preprocessed_img_subj1.nii.gz')
-slice = preprocessed_image.slicer[:, :, :, 0]
-plotting.plot_glass_brain(slice)
+
+# Remove 'rest' images from data
+# TODO: find correct place to apply this
+#pic_voxels, pic_labels = remove_rest(processed_bold_data, labels)
+
+# Sort everything according to labels
+print("Sorting...")
+labels_sorted = labels.sort_values('labels')
+voxels_sorted = processed_bold_data[:, :, :, labels.index]
+
+# create voxel index
+print("Creating voxel index...")
+data_shape = voxels_sorted.shape
+voxel_shape = data_shape[0:3]
+voxel_index_grid = list(np.ndindex(voxel_shape))
+
+# Create list of ROI voxel locations:
+ROI_locs = list(map(tuple, np.argwhere(mask_data == 1)))
+
+# Use only 2 in testing:
+# TODO: remove this when testing done.
+ROI_locs = ROI_locs[0:2]
+
+# Create searchlight areas for each ROI location
+print("Creating searchlights")
+ROI_searchlights = searchlight(ROI_locs, radius, voxel_index_grid)
+
+# create BOLD RDMs for all center voxels:
+ROI_RDMs = {}
+ROI_RDM_progress_bar = Bar('Computing RDMs for ROIs', max=len(ROI_locs))
+
+for loc in ROI_locs:
+
+    ROI_RDMs[loc] = create_bold_RDM(data, loc, ROI_searchlights)
+    ROI_RDM_progress_bar.next()
+
+ROI_RDM_progress_bar.finish()
+
