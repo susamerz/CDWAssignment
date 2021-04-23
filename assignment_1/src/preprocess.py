167dfb1237c8cfe3b19d80ca3703741736061c90
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-04-23 17:37:06 +0300

    Make nan removal optional

--- assignment_1/src/preprocess.py
+++ assignment_1/src/preprocess.py
@@ -3,7 +3,7 @@ from scipy.signal import detrend
 from scipy.stats import zscore
 
 
-def preprocess_data(bd):
+def preprocess_data(bd, zero_nan=False):
     data = bd.data
     processed_data = np.empty_like(data)
     chunks = bd.chunks
@@ -13,8 +13,9 @@ def preprocess_data(bd):
         data_chunk = data[..., chunk_flt]
         data_chunk = detrend(data_chunk, axis=-1)
         data_chunk = zscore(data_chunk, axis=-1)
-        data_chunk[np.isnan(data_chunk)] = 0.0
         processed_data[..., chunk_flt] = data_chunk
+    if zero_nan:
+        processed_data[np.isnan(processed_data)] = 0.0
     return processed_data
 
 
