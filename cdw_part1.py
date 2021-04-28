09c31bb037e73d419714b117eb7cd9b5d02aee20
Laurinoja Joonas <joonas.laurinoja@aalto.fi>
2021-04-28 20:17:17 +0300

    added plotting and tried to fix the pipeline

--- cdw_part1.py
+++ cdw_part1.py
@@ -1,24 +1,20 @@
-from re import search
-from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-import scipy as sc
-from scipy import signal
 import nibabel as nib
 from nilearn import plotting
 from scipy.spatial import distance
 from scipy.stats import spearmanr
+import warnings as wr
+from scipy.signal import detrend
+from scipy.stats import zscore
     
     
 def get_one_RSA(subject_no, path, searchlight_radius):
     
     bold = nib.load(f'{path}/subj{subject_no}/bold.nii.gz')    
     mask = nib.load(f'{path}/subj{subject_no}/mask4_vt.nii.gz')  
-    
     labels_path = f'{path}/subj{subject_no}/labels.txt'
-    
     labels = pd.read_csv(labels_path, sep=' ')  
-    
     bold_data = bold.get_fdata()
     
     chunks = []
@@ -26,9 +22,10 @@ def get_one_RSA(subject_no, path, searchlight_radius):
     group_indices = labels.groupby('chunks').indices   
        
     for _, indices in group_indices.items():
+        
         bold_chunk = np.array(bold_data[:, :, :, indices])
         # preprocess
-        bold_chunk = sc.stats.zscore(sc.signal.detrend(bold_chunk, axis=-1), axis=-1)
+        bold_chunk = zscore(detrend(bold_chunk), axis=-1)
         chunks.append(bold_chunk)
         
     chunks = np.array(chunks)
@@ -47,7 +44,9 @@ def get_one_RSA(subject_no, path, searchlight_radius):
     mRDM = np.zeros((n_images, n_images))
     
     for i in range(mRDM.shape[0]):
+        
         for j in range(mRDM.shape[1]):
+            
             if labels['labels'].values[i] == labels['labels'].values[j]:
                 mRDM[i, j] = 0
             else:
@@ -68,17 +67,16 @@ def get_one_RSA(subject_no, path, searchlight_radius):
     
     bold_data_flat = bold_data.reshape(-1, bold_data.shape[-1])
     
-    for voxel in list(range(searchlight_grid.shape[0])):
-            
-        images = bold_data_flat[searchlight_grid[voxel].nonzero()].T
+    for voxel in range(searchlight_grid.shape[0]):
         
+        images = bold_data_flat[searchlight_grid[voxel].nonzero()].T
         RDM = distance.squareform(distance.pdist(images, metric='correlation'))
         RDMvec = RDM.flatten()
         
         rsa_values[voxel] = spearmanr(mRDMvec, RDMvec)[0]
     
     RSA[mask_data == 1] = rsa_values   
-        
+    
     return RSA
     
 def main():   
@@ -86,6 +84,13 @@ def main():
     # group level analysis: input wanted subject ids
     subject_ids = [1]
     searchlight_radius = 2
+    
+    if len(subject_ids) == 0:
+        wr.warn('No subject IDs.')
+        
+    if searchlight_radius < 0:
+        wr.warn('Searchlight radius has to be non-negative.')
+        
     path = "../CDWAssignment_data"
     
     bold = nib.load(f'{path}/subj{subject_ids[0]}/bold.nii.gz')
@@ -94,14 +99,19 @@ def main():
     allRSA = np.zeros((((len(subject_ids),bold_data.shape[0], bold_data.shape[1], bold_data.shape[2]))))
        
     for index in range(len(subject_ids)):
+        
         allRSA[index,:,:,:] = get_one_RSA(subject_ids[index], path, searchlight_radius)
     
     # compute mean RSA across all subjects
-    mean_RSA = allRSA.mean(axis=0)
     
+    mean_RSA = allRSA.mean(axis=0)
+   
+   
     # ------------------Make image of RSA values in ROI-----------
     RSA_plotting = nib.Nifti1Image(mean_RSA, bold.affine)
-    
     plotting.plot_glass_brain(RSA_plotting)
+    plotting.show()
+    
+    
     
 main()
