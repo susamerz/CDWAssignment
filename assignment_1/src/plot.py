354030e91f4ed6bb8ebbecf7bab4231ecedd59e0
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2021-04-28 10:00:35 +0300

    Add output file for plotting

--- assignment_1/src/plot.py
+++ assignment_1/src/plot.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 from utils import BrainData
 
 
-def main(data_fpath):
+def main(data_fpath, plot_fpath):
     bd = BrainData.from_npz_file(data_fpath)
     bd.plot_brain()
-    plt.show()
+    plt.savefig(plot_fpath)
 
 
 if __name__ == '__main__':
@@ -14,6 +14,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('data_fpath', type=Path)
+    parser.add_argument('plot_fpath', type=Path)
     args = parser.parse_args()
 
     kwargs = vars(args)
